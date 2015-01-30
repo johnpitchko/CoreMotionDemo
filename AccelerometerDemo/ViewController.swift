0cf38beebdb8f99e0e7a38e499277607c468d061
John Pitchko <john@pitchko.com>
2015-01-29 20:20:20 -0700

    Initial Commit

---
+++ AccelerometerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AccelerometerDemo
+//
+//  Created by John Pitchko on 2015-Jan-29.
+//  Copyright (c) 2015 Pitchko Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
